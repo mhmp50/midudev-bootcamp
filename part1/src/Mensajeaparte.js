786bf43f103eb0f2bab650de13994eb653e8e8cc
mhmp50 <mhmp@TOSHIBA.localdomain>
2021-05-06 15:06:24 -0500

    only notes

--- part1/src/Mensajeaparte.js
+++ part1/src/Mensajeaparte.js
@@ -7,3 +7,6 @@ export default Mensajeaparte
 //lo interesante de esto es que a las funciones o componente se les pueden pasar parametros o en REACT se conocen como 'props'
 
 
+// esto es igual a lo de arriba pero en (one line)
+// const Mensajeaparte = () => <h2>este es un mensaje de un modulo creado en un archivo aparte</h2>
+
--- part1/src/Mensajeprops.js
+++ part1/src/Mensajeprops.js
@@ -7,3 +7,4 @@ const Mensajeprops = (props) => {
 }
 
 export default Mensajeprops
+
