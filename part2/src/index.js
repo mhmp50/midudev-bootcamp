d2c3e6f53b6996c065b5a261224d2701bbc03dff
mhmp50 <mhmp@TOSHIBA.localdomain>
2021-05-10 11:54:47 -0500

    evento-contador

--- part2/src/index.js
+++ part2/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import reactDom from 'react-dom';
+//import reactDom from 'react-dom';
 import ReactDOM from 'react-dom';
 import {useState} from "react";   //para usar dentro de nuestro componente
+//import { render } from '@testing-library/react';
 /*import App from './App';*/          //vamos a borrar esto para simplificar y tener todo en este mismo fichero.
 
 const rootElemnt = document.getElementById("root")
@@ -53,7 +54,7 @@ vamos a utilizar es un "HOOK" (gancho) esto nos permite ejecutar codigo en algun
 asi vamos a poder "engancharle un estado a nuestro componente" */
 // 1.)  importamos el "useState" en la parte superior
 
-const App = (props) => {
+//const App = (props) => {
   
   /*let contador  = 0
   setInterval(() => {         //aqui lo que pasa es que el contador se v a seguir incrementando en 1 pero en la pantalla no nos va a parecer mas que el cero. por eso es necesario el gancho "hook"
@@ -62,26 +63,72 @@ const App = (props) => {
   }, 1000)*/
   
   //ahora usemos el "useState" pra no ahcer lo de arriba.
-  const contador = useState(1)        /*como parametro le pasamos el valor inicial del estado. (este se 
+  
+  //const [contadorValue, updateContador] = useState(1)        
+    
+    /*como parametro le pasamos el valor inicial del estado. (este se 
     conforma de dos cosas, una forma de actualizar el estado y el valor del contador). nos devuelve una lista de 
     dos posiciones.  el la primera nos devuelve el valor del estado y en al segunda nos devuelve un metodo que 
     al ejecutarlo podemos actualizar ese estado*/
-  const contadorValue = contador[0]            //el valor del contador
-  const updateContador = contador[1]        //forma de actualizar el estado.
+  
+  /*ahora, estas dos lineas las podemos simplifica con la desestructuracion de objetos y simploficarlo en la
+  misma linea de codigo del "useState"
+  
+  const contadorValue = contador[0]         //el valor del contador
+  const updateContador = contador[1]        //forma de actualizar el estado.*/
 
-  setInterval(() => {
+  /*nunca se debe utilizar un set interval de esta forma, ya que an reenderizar el componente este setInterval tambien se vuelve
+  a ejecutar y es lo que hace qe la aplicacion se "pete", y que los numeros enloquescan, en vez de seguir 
+  incrementando numero por numero. Existe un ejemplo mejor para ver como funcionan los estados en React*/
+  /*setInterval(() => {
     updateContador(contadorValue + 1)
-  }, 2000)
+  }, 2000)*/
 
+  /*console.log(render)  //aqui podemos ver si el componente se reenderiza*/
 
   /*esto es lo valioso de react, aunque tengamos varias etiquetas solo recarga aquella que le indiquemos, o donde
   esta nuestro componente asi no recargamos toda la pagina mara poder mostrar un simple cambio "DOOM VIRTUAL"
   lo que hace react es meter los cambio minimos necesarios en la UI para ver como se refleja el estado*/
-  return (    
+  
+  /*return (    
     <div>
       <p>el valor del contador es </p>
       <h1>{contadorValue}</h1>            
       <h2>magia de react</h2>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, rootElemnt);
+*/
+
+//¿cuando se renderiza el componente? cada vez que el estado cambia y tambien cuendo le llegan "props" nuevas
+
+//------------------------------------------------------------------------------------------------------------------------
+
+/*en ves del "setInterval" podemos usar un evento de click para cada vez que le demos click nos aumente en 
+1 el contador */
+const App = (props) => {
+  
+  const [contadorValue, updateContador] = useState(1)        
+    
+  console.log("render") /*aqui podemos ver que cada vez que hacemos click nos aparece en al consola un "render"
+  asi podemos saber que cada vez que hacemos click el componente se reenderiza nuevamente*/
+
+  return (    
+    <div>
+      <p>el valor del contador es </p>
+      <h1>{contadorValue}</h1>            
+      <button onClick={() => {              //creamos una funcion para llamar añadir un evento a nuestro compoente a travez de un "onClick" y que cada vez que le demos click aumente el contadorValue en 1.
+        //updateContador(contadorValue + 1)   //existe otra forma de actualizar el estado, a updateContador tambien se le puede pasar una funcion por lo que:
+        updateContador(prevContador => {      //aqui creamos un funcion donde le apsamos como parametro el contador previo "preContador" y le decimos que al ejecutarlo no regrese este "prevContador" + 1. la ventaja de realizarzo de esta forma es que nos aseguramos de siempre tener el valor anterior osea "prevContador" y a este sumarle 1 por lo que el resultado sera el siguiente prevContador al que deberemos sumar 1 nuevamente cuando el componente se vuelva a reenderizar.
+          return prevContador + 1;
+        })
+        }
+      }
+      >
+        Incrementar
+      </button>
     </div>
   )
 }
