bcbd213950936d271405299e01e6a82956b700bb
mhmp50 <mhmp@TOSHIBA.localdomain>
2021-05-14 17:30:29 -0500

    newcounter-clicks

--- part2/src/index.js
+++ part2/src/index.js
@@ -16,9 +16,10 @@ const App = (props) => {
   const [counters, setCounters] = useState({
     left: 0,
     right: 0,
-    clicks: 0,    //agregamos un contador para cada click que demos ya sea en el derecho o en el izquierdo
+    //clicks: 0,    //agregamos un contador para cada click que demos ya sea en el derecho o en el izquierdo
     mensaje: "counter mensajirijillo"    //si usamos el codigo de la parte de abajo para manejar estados lo que pasa es que le estamos diciendo que sobreeescriba el estado con lo que tenemos abajo por lo tanto este mensaje si no lo citamos abajo se borrara cuando demos click en algun boton, para esto necesutamos hacer un "...counters" que hara una nueva compia del estado viego y reeemplazara solo aquellos cosas que cambien.  
   })
+  const [clicks, setClicks] = useState([]) //asi podemos contar el numero de clicks que se han dado del boton izquiero y tambien cada click del boton derecho, asi como ademas podemos calcular de aqui mismo el numero total de click con {clicks.length}
 
   const handleClickLeft = () => { //lo que le estamos diciendo es que el nuevo mensaje esto el codigo e abajo por lo que si no citamos todos los elemnetods del array, al momento de ejecutar algo que actualiza el estado borrara lo que no este en este estado asi este en el useState de arriba.
     setCounters({
@@ -27,14 +28,18 @@ const App = (props) => {
       //right: counters.right,    //este elemnento como no se actuliza ya no necesitamos ponerlo en el handle ya que lo traemos con "...counters", asi solo ponemos los elemntos del objeto que queremos se actualicen o se sobreescriban.
       clicks: counters.clicks + 1
     })
+    setClicks(prevClicks => ([...prevClicks, "L"]))
+
   }
 
-  const handleClickRight = () => {
+  const handleClickRight = (event) => {
+    console.log(event)    //hay que tener en cuenta que cuando le damos al boton en <button onClick={handleClickLeft}>left</button> lo que estamos haciendo es mandado esos parametros que los recibe el handle atravez de un "event" si hacemos un console.log a este event nos puede dar informacion muy util y relevante segun lo qu encesitemos.
     setCounters({
       ...counters,
       right: counters.right + 1,
       clicks: counters.clicks + 1
     })
+    setClicks(prevClicks => ([...prevClicks, "R"]))   //para que cada vez que le demos un clicl al boton left, no muestre en la UI un "R" por cada click
   }
 
     return (
@@ -43,9 +48,9 @@ const App = (props) => {
         <button onClick={handleClickLeft}>left</button>
         <button onClick={handleClickRight}>right</button>
         {counters.right}
-        <p>numero de clicks {counters.clicks}</p>
+        <p>numero de clicks {clicks.length}</p>
         <p>{counters.mensaje}</p>
-
+        {clicks.join(', ')}
       </div>
       
     )
@@ -53,3 +58,7 @@ const App = (props) => {
 
 
 ReactDOM.render(<App />, rootElemnt);
+
+
+//.join()  asi podemos agregar un espacio o cualquier cosa que queramos
+//MUY IMPORTANTE: siempre que se pueda tener hayq ue evitar los estados, entremenos se tenga la app sera mas cecncilla. "se debe tener el calculo en el menor numero de sitios". !EVITAR A TODA COSTA TENER ESTADOS QUE NO SE NECESITEN!.
